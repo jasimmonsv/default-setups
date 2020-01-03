d6295d883d0a0d05a690853d049ba92a7df6f2b8
jasimmonsv <simmonsj@jasimmonsv.com>
2020-01-03 16:17:22 -0600

    Add go/bin to $PATH

--- ubuntu/install_go.sh
+++ ubuntu/install_go.sh
@@ -5,9 +5,14 @@ GO_FOLDER=go${GO_VERSION}.linux-amd64
 GO_ARC=${GO_FOLDER}.tar.gz
 GO_LOC=/usr/local
 
+# clean up existing go install
+sudo rm -rf ${GO_LOC}/go
+
+# install defined version
 wget https://dl.google.com/go/${GO_ARC}
 sudo tar xvzf ${GO_ARC} -C ${GO_LOC}
 sudo mv ${GO_LOC}/go ${GO_LOC}/${GO_FOLDER}
 sudo ln -s ${GO_LOC}/${GO_FOLDER} ${GO_LOC}/go
 
+echo 'PATH="$HOME/programming/go/bin:$PATH"' | tee -a "$HOME/.profile"
 rm -rf ${GO_ARC}
